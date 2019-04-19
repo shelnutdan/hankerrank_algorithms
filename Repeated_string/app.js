aec771d57fe8aecdb946a1517d87c50065c85645
shelnutdan <shelnutdaniel@gmail.com>
2019-04-18 21:01:57 -0700

    Added solution to Repeated Strings Algorithm

---
+++ Repeated_string/app.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+Task:Complete the repeatedString function in the editor below. It should return an integer representing the number of occurrences of a in the prefix of length  in the infinitely repeating string.
+
+repeatedString has the following parameter(s):
+
+s: a string to repeat
+n: the number of characters to consider
+*/
+function repeatedString(s, n) {
+    var inString = 0;
+    var added = 0;
+    for (var i = 0; i < s.length; i++) {
+        if (s[i] == 'a') {
+            inString++;
+            if (i < (n % s.length))
+                added++;
+        }
+    }
+    var repeats = (n - n % s.length) / s.length;
+    return repeats * inString +added;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const s = readLine();
+
+    const n = parseInt(readLine(), 10);
+
+    let result = repeatedString(s, n);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
