f13594164b31014fb299a97d3446084f486ebe36
shelnutdan <shelnutdaniel@gmail.com>
2019-04-25 21:00:55 -0700

    Added solution to minimum absolute difference on hankerrank

---
+++ Minimum_abs_diff/app.js
@@ -0,0 +1,66 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+Task: Complete the minimumAbsoluteDifference function in the editor below. It should return an integer that represents the minimum absolute difference between any pair of elements.
+
+minimumAbsoluteDifference has the following parameter(s):
+
+n: an integer that represents the length of arr
+arr: an array of integers
+*/
+function minimumAbsoluteDifference(arr) {
+    arr.sort((a, b) => a - b)
+    let smallestDiff = Math.abs(arr[0] - arr[1]);
+    let absDiff;
+    for (let i = 2; i < arr.length; i++) {
+
+            absDiff = Math.abs(arr[i] - arr[i-1])
+
+            if (absDiff < smallestDiff) {
+                smallestDiff = absDiff;
+            }
+
+
+    }
+
+    return smallestDiff;
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const arr = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const result = minimumAbsoluteDifference(arr);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
