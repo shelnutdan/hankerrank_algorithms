9487816980784403dc2dd05ede747af9f41b4e5a
shelnutdan <shelnutdaniel@gmail.com>
2019-04-29 20:29:06 -0700

    Added solution to Counting valleys problem on hankerrank

---
+++ counting_valleys/app.js
@@ -0,0 +1,70 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+Task:Complete the countingValleys function in the editor below. It must return an integer that denotes the number of valleys Gary traversed.
+
+countingValleys has the following parameter(s):
+
+n: the number of steps Gary takes
+s: a string describing his path
+*/
+function countingValleys(n, s) {
+    let seaLevel = 0;
+    let currLevel = 0;
+    let valleys = 0;
+
+    s = s.split('');
+    s.forEach(element => {
+        if (element === 'U') {
+            currLevel += 1;
+            if (currLevel == 0) {
+                valleys += 1;
+            }
+        } else {
+            currLevel -= 1;
+        }
+
+    })
+
+    return valleys;
+
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const s = readLine();
+
+    let result = countingValleys(n, s);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
