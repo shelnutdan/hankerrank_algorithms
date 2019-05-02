618db678161551b040c02215472b014187a84b5a
shelnutdan <shelnutdaniel@gmail.com>
2019-05-01 20:54:04 -0700

    added solution to Beautiful triples on hankerrank

---
+++ Beautiful_triples/app.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+Task: Complete the beautifulTriplets function in the editor below. It must return an integer that represents the number of beautiful triplets in the sequence.
+
+beautifulTriplets has the following parameters:
+
+d: an integer
+arr: an array of integers, sorted ascending
+*/
+function beautifulTriplets(d, arr) {
+    let counts = {};
+    arr.forEach(x => counts[x] = counts[x] + 1 || 1);
+    return arr.reduce((c, v) => {
+        let a = counts[v - d] * counts[v + d];
+        return c + (isNaN(a) ? 0 : a);
+    }, 0);
+}
+
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const nd = readLine().split(' ');
+
+    const n = parseInt(nd[0], 10);
+
+    const d = parseInt(nd[1], 10);
+
+    const arr = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    let result = beautifulTriplets(d, arr);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
